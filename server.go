1b727b44fc53974c22c2bc2822b430a525bf51e6
kaoudis <kaoudis@colorado.edu>
2017-03-21 17:13:11 -0600

    now we can even wait

--- server.go
+++ server.go
@@ -4,17 +4,30 @@ import (
         "fmt"
 	"log"
 	"net/http"
+        "time"
 )
 
 func main() {
     router := http.NewServeMux()
 
-    router.HandleFunc("/", waitAndHash)
+    router.HandleFunc("/", hasher)
 
-    err := http.ListenAndServe(":8080", router)
+    waitingRouter := waiter(router)
+
+    err := http.ListenAndServe(":8080", waitingRouter)
     log.Fatal(err)
 }
 
-func waitAndHash(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "hello friendly friends")
+func hasher(writer http.ResponseWriter, request *http.Request) {
+    fmt.Fprintln(writer, "!!!!!!!!!!!!!!!!!1!!!!1!! HASHES COMING SOON !!!!!!!!!!!!!!!!1!!!!1!!!!!")
 }
+
+func waiter(handler http.Handler) http.Handler {
+    return http.HandlerFunc(
+        func(writer http.ResponseWriter, request *http.Request) {
+            time.Sleep(time.Duration(5) * time.Second)
+            handler.ServeHTTP(writer, request)
+        })
+
+}
+
